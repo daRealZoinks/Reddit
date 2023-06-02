d75b6e2523ad1fea2164f21060ccbee87f7f588b
petreastefann <petreastefann@gmail.com>
2023-06-02 23:08:01 +0300

    added Comment and its Dto

---
+++ API/DataLayer/Dtos/CommentDto.cs
@@ -0,0 +1,19 @@
+﻿namespace DataLayer.Dtos;
+
+public class CommentDto {
+	public int Id {
+		get; set;
+	}
+	public DateTime PostDate {
+		get; set;
+	}
+	public string Content {
+		get; set;
+	}
+	public int PostId {
+		get; set;
+	}
+	public int UserId {
+		get; set;
+	}
+}
---
+++ API/DataLayer/Entities/Comment.cs
@@ -0,0 +1,31 @@
+﻿namespace DataLayer.Entities;
+
+public class Comment {
+	public int Id {
+		get; set;
+	}
+
+	public DateTime PostDate {
+		get; set;
+	}
+
+	public string Content {
+		get; set;
+	}
+
+	public int PostId {
+		get; set;
+	}
+
+	public Post Post {
+		get; set;
+	}
+
+	public int UserId {
+		get; set;
+	}
+
+	public User User {
+		get; set;
+	}
+}
