2f381d595c4ef40c0c9fb23f5b619a69440fa2e8
petreastefann <petreastefann@gmail.com>
2023-06-24 16:32:45 +0300

    added Vote/VoteDto classes and the Votes static class for accessing the routes

--- Client/RedditPublicAPI/Entities/Post.cs
+++ Client/RedditPublicAPI/Entities/Post.cs
@@ -1,19 +1,31 @@
 ﻿namespace RedditPublicAPI.Entities;
 
-public class Post : BaseEntity
-{
-    public DateTimeOffset PostDate { get; set; }
-    public string Title { get; set; }
-    public string Content { get; set; }
+public class Post : BaseEntity {
+	public DateTimeOffset PostDate {
+		get; set;
+	}
+	public string Title {
+		get; set;
+	}
+	public string Content {
+		get; set;
+	}
 
-    public int AuthorId { get; set; }
-    public User Author { get; set; }
+	public int AuthorId {
+		get; set;
+	}
+	public User Author {
+		get; set;
+	}
 
-    public int CommunityId { get; set; }
-    public Community Community { get; set; }
+	public int CommunityId {
+		get; set;
+	}
+	public Community Community {
+		get; set;
+	}
 
-    public override string ToString()
-    {
-        return Title;
-    }
+	public override string ToString() {
+		return Title;
+	}
 }
