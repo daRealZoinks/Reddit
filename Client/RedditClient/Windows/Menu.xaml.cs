2c57cce302603ba0f09a4d8b57856053f66b14d8
petreastefann <petreastefann@gmail.com>
2023-06-24 16:36:42 +0300

    added the crud window and tested it

--- Client/RedditClient/Windows/Menu.xaml.cs
+++ Client/RedditClient/Windows/Menu.xaml.cs
@@ -2,58 +2,53 @@
 
 namespace RedditClient.Windows;
 
-public partial class Menu : Window
-{
-    public Menu()
-    {
-        InitializeComponent();
-    }
+public partial class Menu : Window {
+	public Menu() {
+		InitializeComponent();
+	}
 
-    private void UsersButton_Click(object sender, RoutedEventArgs e)
-    {
-        UserCrud userCrud = new();
-        userCrud.Show();
-    }
+	private void UsersButton_Click(object sender, RoutedEventArgs e) {
+		UserCrud userCrud = new();
+		userCrud.Show();
+	}
 
-    private void MessagesButton_Click(object sender, RoutedEventArgs e)
-    {
-        MessageCrud messageCrud = new();
-        messageCrud.Show();
-    }
+	private void MessagesButton_Click(object sender, RoutedEventArgs e) {
+		MessageCrud messageCrud = new();
+		messageCrud.Show();
+	}
 
-    private void AchievementsButton_Click(object sender, RoutedEventArgs e)
-    {
-        AchievementCrud achievementCrud = new();
-        achievementCrud.Show();
-    }
+	private void AchievementsButton_Click(object sender, RoutedEventArgs e) {
+		AchievementCrud achievementCrud = new();
+		achievementCrud.Show();
+	}
 
-    private void CommunitiesButton_Click(object sender, RoutedEventArgs e)
-    {
-        CommunityCrud communityCrud = new();
-        communityCrud.Show();
-    }
+	private void CommunitiesButton_Click(object sender, RoutedEventArgs e) {
+		CommunityCrud communityCrud = new();
+		communityCrud.Show();
+	}
 
-    private void PostsButton_Click(object sender, RoutedEventArgs e)
-    {
-        PostCrud postCrud = new();
-        postCrud.Show();
-    }
+	private void PostsButton_Click(object sender, RoutedEventArgs e) {
+		PostCrud postCrud = new();
+		postCrud.Show();
+	}
 
-    private void CommentsButton_Click(object sender, RoutedEventArgs e)
-    {
-        CommentCrud commentCrud = new();
-        commentCrud.Show();
-    }
+	private void CommentsButton_Click(object sender, RoutedEventArgs e) {
+		CommentCrud commentCrud = new();
+		commentCrud.Show();
+	}
 
-    private void CommunityUsersButton_Click(object sender, RoutedEventArgs e)
-    {
-        CommunityUserWindow communityUserWindow = new();
-        communityUserWindow.Show();
-    }
+	private void VotesButton_Click(object sender, RoutedEventArgs e) {
+		VoteCrud voteCrud = new();
+		voteCrud.Show();
+	}
 
-    private void AchievementUsersButton_Click(object sender, RoutedEventArgs e)
-    {
-        AchievementUserWindow achievementUserWindow = new();
-        achievementUserWindow.Show();
-    }
+	private void CommunityUsersButton_Click(object sender, RoutedEventArgs e) {
+		CommunityUserWindow communityUserWindow = new();
+		communityUserWindow.Show();
+	}
+
+	private void AchievementUsersButton_Click(object sender, RoutedEventArgs e) {
+		AchievementUserWindow achievementUserWindow = new();
+		achievementUserWindow.Show();
+	}
 }
---
+++ Client/RedditClient/Windows/VoteCrud.xaml.cs
@@ -0,0 +1,95 @@
+﻿using RedditPublicAPI;
+using RedditPublicAPI.Entities;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RedditClient.Windows;
+
+public partial class VoteCrud : Window {
+	private readonly ObservableCollection<Vote> _votes = new();
+
+	public VoteCrud() {
+		InitializeComponent();
+		VotesListView.ItemsSource = _votes;
+
+		Initialize();
+	}
+
+	private async void Initialize() {
+		UserComboBox.ItemsSource = await Users.GetUsers(App.Token);
+		PostComboBox.ItemsSource = await Posts.GetPosts(App.Token);
+	}
+
+	private async void GetButton_Click(object sender, RoutedEventArgs e) {
+		try {
+			var votes = await Votes.GetVotes(App.Token);
+
+			_votes.Clear();
+
+			if(votes is not null)
+				foreach(var vote in votes)
+					_votes.Add(vote);
+		}
+		catch(Exception ex) {
+			MessageBox.Show(ex.Message);
+		}
+	}
+
+	private async void CreateButton_Click(object sender, RoutedEventArgs e) {
+		try {
+			Vote vote = new() {
+				Upvote = UpvoteCheckBox.IsChecked ?? false,
+				UserId = ((User)UserComboBox.SelectedItem).Id,
+				PostId = ((Post)PostComboBox.SelectedItem).Id
+			};
+
+			await Votes.AddVote(vote, App.Token);
+
+			_votes.Add(vote);
+		}
+		catch(Exception ex) {
+			MessageBox.Show(ex.Message);
+		}
+	}
+
+	private void UpdateButton_Click(object sender, RoutedEventArgs e) {
+		try {
+			if(VotesListView.SelectedValue is not Vote vote)
+				return;
+
+			vote.Upvote = UpvoteCheckBox.IsChecked ?? false;
+			vote.UserId = ((User)UserComboBox.SelectedItem).Id;
+			vote.PostId = ((Post)PostComboBox.SelectedItem).Id;
+
+			Votes.UpdateVote(vote, App.Token);
+		}
+		catch(Exception ex) {
+			MessageBox.Show(ex.Message);
+		}
+	}
+
+	private void DeleteButton_Click(object sender, RoutedEventArgs e) {
+		try {
+			if(VotesListView.SelectedValue is not Vote vote)
+				return;
+
+			Votes.DeleteVote(vote, App.Token);
+
+			_votes.Remove(vote);
+		}
+		catch(Exception ex) {
+			MessageBox.Show(ex.Message);
+		}
+	}
+
+	private void VotesListView_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+		if(VotesListView.SelectedValue is not Vote vote)
+			return;
+
+		UpvoteCheckBox.IsChecked = vote.Upvote;
+		UserComboBox.SelectedItem = vote.User;
+		PostComboBox.SelectedItem = vote.Post;
+	}
+}
